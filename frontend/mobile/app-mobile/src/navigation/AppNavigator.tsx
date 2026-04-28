84f4d9e53769fb30139391471d696af7abe5547d
Pablo_Ruiz <pabloruizcar@gmail.com>
2026-04-28 18:00:09 +0200

    Add HomeScreen and prepare the future of the screens

--- frontend/mobile/app-mobile/src/navigation/AppNavigator.tsx
+++ frontend/mobile/app-mobile/src/navigation/AppNavigator.tsx
@@ -18,7 +18,7 @@ export default function AppNavigator() {
           options={{ headerShown: false }}
         />
 
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}  options={{ headerShown: false }}/>
 
         <Stack.Screen
           name="Register"
